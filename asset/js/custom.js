7556898f06413ffde0d2854172e142296f8f29e6
devshisir <contact.shisirimran@gmail.com>
2020-11-02 17:02:22 +0000

    Tab Component

---
+++ asset/js/custom.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  var Fullwindow = $(window);
+  var page = $('html, body');
+
+
+
+  if ($(".accordion__item__header").length > 0) {
+    var active = "active";
+    $(".accordion__item__header").click(function () {
+      $(this).toggleClass(active);
+      $(this).next("div").slideToggle(200);
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
